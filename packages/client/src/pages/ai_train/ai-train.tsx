c828f652d924971157540b05784e510f539f9b40
Dveyn <ananiev_dima.96@mail.ru>
2023-09-20 23:08:15 +0300

    основа фронта и обучения

---
+++ packages/client/src/pages/ai_train/ai-train.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const AITrain = () => {
+  return <div>AI</div>;
+};
--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,14 +1,25 @@
 import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import { trainModel } from './model/trainModel'; // Импортируем функцию для обучения модели
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Обработчик для корневого маршрута
-app.get('/', (req: Request, res: Response) => {
-  res.send('Привет, мир!');
+app.use(bodyParser.json());
+
+// Ручка для принятия данных и запуска обучения модели
+app.post('/train', (req: Request, res: Response) => {
+  const inputData = req.body.data; // Данные для обучения (тексты статей)
+  const inputLabels = req.body.labels; // Метки классов
+
+  trainModel(inputData, inputLabels); // Запускаем обучение модели
+
+  res.send('Обучение начато');
 });
 
-// Запуск сервера
+//Ручка для получения значения обучаемости
+
+
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
