dd28119d73c7362d68dfa1ef509e08cb841d4d61
Dveyn <ananiev_dima.96@mail.ru>
2023-12-26 23:54:35 +0300

    [ADD] header pages

--- packages/client/src/components/base/header/header.tsx
+++ packages/client/src/components/base/header/header.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import styles from "./header.module.css";
 
 export const Header = () => {
-  return <></>
+  const handleMouseEnterLeave = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    const elem = e.currentTarget as HTMLLIElement;
+    const tolerance = 10;
+    const left = 0;
+    const right = elem.clientWidth;
+    let x = e.pageX - elem.offsetLeft;
+
+    if (x - tolerance < left) x = left
+    if (x + tolerance > right) x = right
+
+    elem.style.setProperty('--x', `${x}px`)
+  };
+
+  return (
+    <header className={styles.header}>
+      <a href="/" className={styles.logo}>
+        <img src="/media/audit_logo.png" alt="audit boost" />
+      </a>
+      <nav className={styles.nav} >
+        <ul>
+          <li className={styles.item} onMouseEnter={handleMouseEnterLeave} onMouseLeave={handleMouseEnterLeave}>Дашборд</li>
+          <li className={styles.item} onMouseEnter={handleMouseEnterLeave} onMouseLeave={handleMouseEnterLeave}>Тарифы и цены</li>
+          <li className={styles.item} onMouseEnter={handleMouseEnterLeave} onMouseLeave={handleMouseEnterLeave}>Сообщество</li>
+          <li className={styles.item} onMouseEnter={handleMouseEnterLeave} onMouseLeave={handleMouseEnterLeave}>Блог</li>
+          <li className={styles.item} onMouseEnter={handleMouseEnterLeave} onMouseLeave={handleMouseEnterLeave}>О нас</li>
+          <li className={styles.item} onMouseEnter={handleMouseEnterLeave} onMouseLeave={handleMouseEnterLeave}>Войти</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 
+import { Header } from '@@/components/base/header/header'
 import '@@/styles/globals.css'
 import { AuthWrapper } from '@@/unitls/authWrapper'
 import type { AppProps } from 'next/app'
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <AuthWrapper>
+        <Header />
         <Component {...pageProps} />
       </AuthWrapper>
     </>
